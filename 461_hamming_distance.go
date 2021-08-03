79b2a4c026448f571098f3a05ff2cde9820c4def
teerapat game <teerapat12345678@gmail.com>
2021-08-03 10:08:53 +0700

    learning hamming distance

---
+++ 461_hamming_distance.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "fmt"
+
+func hammingDistance(x int, y int) int {
+	if x
+	fmt.Println("x", x)
+	fmt.Println("y", y)
+	fmt.Println("x", x>>1)
+	fmt.Println("y", y>>1)
+	fmt.Println("x", x>>1>>1)
+	fmt.Println("y", y>>1>>1)
+	fmt.Println("x", x>>1>>1>>1)
+	fmt.Println("y", y>>1>>1>>1)
+	fmt.Println("x", x>>1>>1>>1>>1)
+	fmt.Println("y", y>>1>>1>>1>>1)
+	return 0
+}
---
+++ 461_hamming_distance_test.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import "testing"
+
+func Test_hammingDistance(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				x: 1,
+				y: 4,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingDistance(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("hammingDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
