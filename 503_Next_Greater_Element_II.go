ae693ee49c444021bce3bd5105bf29641dfbf1ab
Teerapat Thong-O <teerapat@gopomelo.com>
2021-08-10 16:50:08 +0700

    Done Fucking nextGreaterElements, it took about 14 hours

--- 503_Next_Greater_Element_II.go
+++ 503_Next_Greater_Element_II.go
@@ -2,13 +2,60 @@ package leetcode
 
 import "fmt"
 
-func nextGreaterElements(nums []int) []int {
-	max := nums[0]
-	for _, v := range nums {
-		if v > max {
-			max = v
+type Stack struct {
+	items []Item
+}
+type Item struct {
+	value int
+	pos   int
+}
+
+// Push
+func (s *Stack) Push(item Item) {
+	s.items = append(s.items, item)
+}
+
+// Pop
+func (s *Stack) Pop() Item {
+	lenStack := len(s.items)
+	if lenStack == 0 {
+		return Item{
+			pos: -1,
 		}
 	}
-	fmt.Println(max)
-	return []int{}
+	toRemove := s.items[lenStack-1]
+	s.items = s.items[:lenStack-1]
+	return toRemove
+}
+
+func nextGreaterElements(nums []int) []int {
+
+	myStack := Stack{}
+	tempStack := Stack{}
+	lenNums := len(nums)
+
+	nums = append(nums, nums...)
+	for i := 0; i < len(nums); i++ {
+		for len(myStack.items) > 0 {
+			item := myStack.Pop()
+			if item.value < nums[i] {
+				nums[item.pos] = nums[i]
+			} else {
+				tempStack.Push(item)
+				break
+			}
+		}
+		fmt.Println(tempStack)
+		for len(tempStack.items) > 0 {
+			item := tempStack.Pop()
+			myStack.Push(item)
+		}
+		myStack.Push(Item{
+			value: nums[i],
+			pos:   i,
+		})
+		nums[i] = -1
+	}
+
+	return nums[:lenNums]
 }
--- 503_Next_Greater_Element_II_test.go
+++ 503_Next_Greater_Element_II_test.go
@@ -14,14 +14,48 @@ func Test_nextGreaterElements(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		// {
+		// 	name: "test1",
+		// 	args: args{
+		// 		nums: []int{4, 2, 3, 1, 5},
+		// 	},
+		// 	want: []int{5, 3, 5, 5, -1},
+		// },
+		// {
+		// 	name: "test2",
+		// 	args: args{
+		// 		nums: []int{1, 2, 1},
+		// 	},
+		// 	want: []int{2, -1, 2},
+		// },
+		// {
+		// 	name: "test3",
+		// 	args: args{
+		// 		nums: []int{1, 2, 3, 4, 3},
+		// 	},
+		// 	want: []int{2, 3, 4, -1, 4},
+		// },
+		// {
+		// 	name: "test4",
+		// 	args: args{
+		// 		nums: []int{1, 2, 3, 2, 1},
+		// 	},
+		// 	want: []int{2, 3, -1, 3, 2},
+		// },
 		{
-			name: "test1",
+			name: "test5",
 			args: args{
-				nums: []int{1, 2, 1},
+				nums: []int{1, 8, -1, -100, -1, 222},
 			},
-			want: []int{2, -1, 2},
+			want: []int{8, 222, 222, -1, 222, -1},
 		},
+		// {
+		// 	name: "test6",
+		// 	args: args{
+		// 		nums: []int{1, 8, -1, -100, -1, 222, 1111111, -111111},
+		// 	},
+		// 	want: []int{8, 222, 222, -1, 222, 1111111, -1, 1},
+		// },
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
