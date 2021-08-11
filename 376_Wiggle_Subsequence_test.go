35e07a8a43a5c6abccc85ccf7f794f676e9b44c1
Teerapat Thong-O <teerapat@gopomelo.com>
2021-08-11 15:46:04 +0700

    done logic wiggleMaxLength

---
+++ 376_Wiggle_Subsequence_test.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import "testing"
+
+func Test_wiggleMaxLength(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				nums: []int{1, 7, 4, 9, 2, 5},
+			},
+			want: 6,
+		},
+		{
+			name: "test2",
+			args: args{
+				nums: []int{1, 17, 5, 10, 13, 15, 10, 5, 16, 8},
+			},
+			want: 7,
+		},
+		{
+			name: "test3",
+			args: args{
+				nums: []int{0, 0},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := wiggleMaxLength(tt.args.nums); got != tt.want {
+				t.Errorf("wiggleMaxLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
