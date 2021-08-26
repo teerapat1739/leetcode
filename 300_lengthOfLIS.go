3d3b25f0ad98764cd414abbb9f433ea8cba7fb2e
GameInSpace <teerapat12345678@gmail.com>
2021-08-26 14:07:19 +0700

    Success Runtime: 1280 ms, faster than 5.01% of Go online submissions for Longest Increasing Subsequence. Memory Usage: 7.1 MB, less than 5.01% of Go online submissions for Longest Increasing Subsequence.

---
+++ 300_lengthOfLIS.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import "fmt"
+
+type Sub struct {
+	val   int
+	index int
+	lis   int
+}
+
+/*
+Success
+Runtime: 1280 ms, faster than 5.01% of Go online submissions for Longest Increasing Subsequence.
+Memory Usage: 7.1 MB, less than 5.01% of Go online submissions for Longest Increasing Subsequence.
+
+*/
+func lengthOfLIS(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	max := 1
+	m := make(map[int]Sub)
+	for i := 0; i < length; i++ {
+		m[i] = Sub{nums[i], i, 1}
+		// j := m[i].index
+		for j := i - 1; j >= 0; j-- {
+			if nums[i] > nums[j] {
+				a := Max(m[i].lis, m[j].lis+1)
+				m[i] = Sub{nums[i], i, a}
+				max = Max(max, a)
+				// break
+			}
+		}
+	}
+	fmt.Println(m)
+	return max
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
