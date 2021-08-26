bf799dd4ae075b83ad9294e9ffb18d1925180fbb
GameInSpace <teerapat12345678@gmail.com>
2021-08-26 14:40:28 +0700

    use: dynamic programing Success Runtime: 532 ms, faster than 5.01% of Go online submissions for Longest Increasing Subsequence. Memory Usage: 6.4 MB, less than 5.01% of Go online submissions for Longest Increasing

--- 300_lengthOfLIS.go
+++ 300_lengthOfLIS.go
@@ -37,6 +37,39 @@ func lengthOfLIS(nums []int) int {
 	return max
 }
 
+/*
+use: dynamic programing
+Success
+Runtime: 532 ms, faster than 5.01% of Go online submissions for Longest Increasing Subsequence.
+Memory Usage: 6.4 MB, less than 5.01% of Go online submissions for Longest Increasing
+*/
+
+func lengthOfLIS2(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	max := 1
+	m := make(map[int]Sub)
+	for i := 0; i < length; i++ {
+		m[i] = Sub{nums[i], i, 1}
+		// j := m[i].index
+		for j := i - 1; j >= 0; j-- {
+			if nums[i] > nums[j] {
+				a := Max(m[i].lis, m[j].lis+1)
+				m[i] = Sub{nums[i], i, a}
+				if a > max {
+					max = Max(max, a)
+					break
+				}
+				max = Max(max, a)
+			}
+		}
+	}
+	fmt.Println(m)
+	return max
+}
+
 func Max(a, b int) int {
 	if a > b {
 		return a
