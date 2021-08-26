51aa270622619a335260912c793327f3f25a5c75
GameInSpace <teerapat12345678@gmail.com>
2021-08-26 20:59:34 +0700

    Success Details Runtime: 9 ms, faster than 8.29% of Go online submissions for Best Time to Buy and Sell Stock II. Memory Usage: 3.3 MB, less than 9.01% of Go online submissions for Best Time to Buy and Sell Stock II.

---
+++ 122_Best_Time_to_Buy_and Sell_Stock_II.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/*
+Success
+Details
+Runtime: 9 ms, faster than 8.29% of Go online submissions for Best Time to Buy and Sell Stock II.
+Memory Usage: 3.3 MB, less than 9.01% of Go online submissions for Best Time to Buy and Sell Stock II.
+
+*/
+func maxProfitII(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	bestValue := 0
+	for i := 1; i < len(prices); i++ {
+
+		if prices[i] > prices[i-1] {
+			bestValue += prices[i] - prices[i-1]
+		}
+	}
+	return bestValue
+}
