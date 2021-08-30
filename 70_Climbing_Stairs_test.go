ac43b174a774a2cc5e88a9d4ae73f067b2b172a7
GameInSpace <teerapat12345678@gmail.com>
2021-08-30 14:15:53 +0700

    success with dynamicprograming

--- 70_Climbing_Stairs_test.go
+++ 70_Climbing_Stairs_test.go
@@ -29,7 +29,7 @@ func Test_climbStairs(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := climbStairs(tt.args.n); got != tt.want {
+			if got := climbStairs2(tt.args.n); got != tt.want {
 				t.Errorf("climbStairs() = %v, want %v", got, tt.want)
 			}
 		})
