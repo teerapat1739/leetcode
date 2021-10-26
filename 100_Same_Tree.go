3817a02f88d8f1f42c3d6d1a03be179d530b0d9f
GameInSpace <teerapat12345678@gmail.com>
2021-10-26 08:15:44 +0700

    Runtime: 0 ms, faster than 100.00% of Go online submissions for Same Tree. Memory Usage: 2.1 MB, less than 59.09% of Go online submissions for Same Tree.

---
+++ 100_Same_Tree.go
@@ -0,0 +1,31 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Same Tree.
+Memory Usage: 2.1 MB, less than 59.09% of Go online submissions for Same Tree.
+*/
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
