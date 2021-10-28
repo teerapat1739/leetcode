ef208ace90f3e01007a6dc0879b1762579aedddc
GameInSpace <teerapat12345678@gmail.com>
2021-10-28 10:20:32 +0700

    Runtime: 28 ms, faster than 94.79% of Go online submissions for 3Sum. Memory Usage: 7.6 MB, less than 64.38% of Go online submissions for 3Sum.

---
+++ 15_3Sum.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import "sort"
+
+/*
+Runtime: 28 ms, faster than 94.79% of Go online submissions for 3Sum.
+Memory Usage: 7.6 MB, less than 64.38% of Go online submissions for 3Sum.
+*/
+func threeSum1(nums []int) [][]int {
+	sort.Ints(nums)
+	var res [][]int
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			if nums[i]+nums[left]+nums[right] == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if nums[i]+nums[left]+nums[right] < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return res
+}
