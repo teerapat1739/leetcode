30e5c8d1468657f6ae9abd8c3a02bcfe90e305e0
GameInSpace <teerapat12345678@gmail.com>
2021-11-02 10:25:36 +0700

    Runtime: 0 ms, faster than 100.00% of Go online submissions for Unique Paths II. Memory Usage: 2.6 MB, less than 76.44% of Go online submissions for Unique Paths II.

--- 63_Unique_Paths_II.go
+++ 63_Unique_Paths_II.go
@@ -1,5 +1,9 @@
 package leetcode
 
+/*
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Unique Paths II.
+Memory Usage: 2.6 MB, less than 76.44% of Go online submissions for Unique Paths II.
+*/
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	dp := make([][]int, len(obstacleGrid))
 	for i := 0; i < len(obstacleGrid); i++ {
