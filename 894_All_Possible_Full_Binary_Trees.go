ddfb56c11c7adac6d6e58264481b348ab0cdeabf
GameInSpace <teerapat12345678@gmail.com>
2021-11-27 11:01:00 +0700

    Runtime: 16 ms, faster than 88.10% of Go online submissions for All Possible Full Binary Trees. Memory Usage: 9.4 MB, less than 57.14% of Go online submissions for All Possible Full Binary Trees.

--- 894_All_Possible_Full_Binary_Trees.go
+++ 894_All_Possible_Full_Binary_Trees.go
@@ -39,3 +39,36 @@ func allPossibleFBT(n int) []*TreeNode {
 	}
 	return res
 }
+
+/*
+Runtime: 16 ms, faster than 88.10% of Go online submissions for All Possible Full Binary Trees.
+Memory Usage: 9.4 MB, less than 57.14% of Go online submissions for All Possible Full Binary Trees.
+*/
+var memo = make([][]*TreeNode, 20+1)
+
+func allPossibleFBTV2(n int) []*TreeNode {
+	if n == 1 {
+		return []*TreeNode{&TreeNode{Val: 0}}
+	}
+	if memo[n] != nil {
+		return memo[n]
+	}
+
+	res := []*TreeNode{}
+	for i := 1; i < n; i++ {
+		left := allPossibleFBTV2(i)
+		right := allPossibleFBTV2(n - i - 1)
+
+		for _, l := range left {
+			for _, r := range right {
+				root := &TreeNode{Val: 0}
+				root.Left = l
+				root.Right = r
+				res = append(res, root)
+			}
+		}
+	}
+
+	memo[n] = res
+	return res
+}
